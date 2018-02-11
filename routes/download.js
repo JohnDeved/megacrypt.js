6c797bd2afa1e24969b748d2cfc7b15f4c14537e
DevSec <arma.devsec@gmail.com>
2018-02-11 16:12:14 +0100

    ~ new file

--- routes/download.js
+++ routes/download.js
@@ -4,17 +4,16 @@ const router = express.Router()
 const megacrypt = require('../modules/megacrypt.js')
 
 router.get('/:crypt/:key', function (req, res, next) {
-  // https://mega.nz/#!ZckkgZTa!FexZJB-KkPMOrM1qT3cU_5Vqd77531F9RvxHNnAJ2QA
   console.log(req.params)
 
   let decrypt = megacrypt.decryptUrl(req.params.crypt, req.params.key)
-  let file = mega.File.fromURL(`https://mega.nz/#!${decrypt.fileId}!${decrypt.fileKey}`)
+  let file = new mega.File({downloadId: decrypt.fileId, key: decrypt.fileKey, directory: false})
 
   file.loadAttributes((err, file) => {
     if (err) throw err
     res.setHeader('Content-Length', file.size)
     res.setHeader('Content-Disposition', `attachment; filename="${file.name}"`)
-    file.download().pipe(res)
+    file.download(/*{returnCiphertext: true}*/).pipe(res)
   })
 })
 
