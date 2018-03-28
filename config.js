a30d6c82f46d5af14c00694339bda58ae28fb9b8
DevSec <arma.devsec@gmail.com>
2018-03-28 18:52:41 +0200

    + fix host+port

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 module.exports = {
+    // set if domain without port is use as host
+  domain: false,
     // Set Host
-    host: '127.0.0.1',
+  host: '127.0.0.1',
     // The port megacrypt.js will be running on
-    port: '3427',
+  port: '3427',
     // Server Decryption Key (change this if hosted)
-    serverKey: "PrkqcWVp@/-,R%2P/kEWC-'PgPMHFFM@>EF4x5J-5]}=-:N%rm${dVLf5@3F]/=2",
+  serverKey: "PrkqcWVp@/-,R%2P/kEWC-'PgPMHFFM@>EF4x5J-5]}=-:N%rm${dVLf5@3F]/=2",
     // if true, the link encryption tool will on the base url
-    encryptionTool: true,
+  encryptionTool: true,
     // if true, the link encryption api will be enabled (nedded for the encryption tool)
-    encrytionApi: true,
+  encrytionApi: true,
     // if true, the site will return the requested files without decryption them
-    returnCiphertext: false
+  returnCiphertext: false
 }
--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const megacrypt = require('../modules/megacrypt.js')
 const config = require('../config')
-router.post('/encrypt', function(req, res, next) {
-    if (req.body.url && config.encrytionApi) {
-        megacrypt.encryptUrl(req.body.url, config.host, response => {
-            res.send(response)
-        })
-    }
+router.post('/encrypt', function (req, res, next) {
+  if (req.body.url && config.encrytionApi) {
+    megacrypt.encryptUrl(req.body.url, config.domain ? config.host : `${config.host}:${config.port}`, response => {
+      res.send(response)
+    })
+  }
 })
 module.exports = router
