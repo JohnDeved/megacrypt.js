6c3f469b4bb69620541344cff6332343a5f806ef
DevSec <arma.devsec@gmail.com>
2018-03-28 19:08:04 +0200

    + minor ui fixes

--- public/javascripts/crypter.js
+++ public/javascripts/crypter.js
@@ -1,13 +1,13 @@
-new ClipboardJS('.btn');
-$('#send').click(function() {
-    $('#urls').val().split(String.fromCharCode('10')).forEach(el => {
-        $.post('/api/encrypt', {
-            url: el
-        }, function(data) {
-            console.log(data)
-            data.forEach(el => {
-                $('#crypt').append(' <p>' + el.name + ': [' + el.size + ']' + ' </p><p><a target="_blank" class="btn btn-default" href="' + String.fromCharCode('10') + el.link + String.fromCharCode('10') + String.fromCharCode('10') + '">Download Link</a> <button data-clipboard-text="' + String.fromCharCode('10') + el.link + String.fromCharCode('10') + String.fromCharCode('10') + '" class="btn btn-default">Copy</button> </p>')
-            })
-        })
+const clipboard = new ClipboardJS('.copy')
+$('#send').click(function () {
+  $('#urls').val().split(String.fromCharCode('10')).forEach(el => {
+    $.post('/api/encrypt', {
+      url: el
+    }, function (data) {
+      console.log(data)
+      data.forEach(el => {
+        $('#crypt').append(' <p>' + el.name + ': [' + el.size + ']' + ' </p><p><a target="_blank" class="btn btn-default" href="' + el.link + '">Download Link</a> <button data-clipboard-text="' + el.link + '" class="btn btn-default copy">Copy</button> </p>')
+      })
     })
+  })
 })
