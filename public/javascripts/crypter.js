f4a010508dcf795be86be597d49e228e56391670
xam <admin@xbitestudio.com>
2018-03-28 01:59:42 -0500

    UI and Copytoclipboard

--- public/javascripts/crypter.js
+++ public/javascripts/crypter.js
@@ -1,10 +1,13 @@
-$('#send').click(function () {
-  $('#urls').val().split(String.fromCharCode('10')).forEach(el => {
-    $.post('/api/encrypt', { url: el }, function (data) {
-      console.log(data)
-      data.forEach(el => {
-        $('#crypt').val($('#crypt').val() + el.name + ' [' + el.size + ']' + String.fromCharCode('10') + el.link + String.fromCharCode('10') + String.fromCharCode('10'))
-      })
+new ClipboardJS('.btn');
+$('#send').click(function() {
+    $('#urls').val().split(String.fromCharCode('10')).forEach(el => {
+        $.post('/api/encrypt', {
+            url: el
+        }, function(data) {
+            console.log(data)
+            data.forEach(el => {
+                $('#crypt').append(' <p>' + el.name + ': [' + el.size + ']' + ' </p><p><a target="_blank" class="btn btn-default" href="' + String.fromCharCode('10') + el.link + String.fromCharCode('10') + String.fromCharCode('10') + '">Download Link</a> <button data-clipboard-text="' + String.fromCharCode('10') + el.link + String.fromCharCode('10') + String.fromCharCode('10') + '" class="btn btn-default">Copy</button> </p>')
+            })
+        })
     })
-  })
 })
